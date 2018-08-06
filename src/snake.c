17bf3ecf6a954f5e21dc035eaabd38f24ee0ed85
sradley <stephen.t.radley@gmail.com>
2018-08-06 14:42:28 +1000

    updated snake.c and snake.h

--- src/snake.c
+++ src/snake.c
@@ -5,6 +5,16 @@ snake.c
 #include <snake.h>
 
 /* typedefs and structs */
+typedef struct snake_node {
+    int          x;
+    int          y;
+    snake_node_t *next;
+} snake_node_t;
+
+typedef struct food {
+    int x;
+    int y;
+} food_t;
 
 
 int main() {
--- src/snake.h
+++ src/snake.h
@@ -3,5 +3,20 @@
 
 #include <ncurses.h>
 #include <unistd.h>
+#include <time.h>
+#include <stdlib.h>
+
+
+/* constants */
+#define SNAKE_START_LEN 1
+
+
+/* typedefs */
+typedef struct snake_node snake_node_t;
+typedef struct food food_t;
+
+
+/* function declarations */
+
 
 #endif  /* SNAKE_H_ */
